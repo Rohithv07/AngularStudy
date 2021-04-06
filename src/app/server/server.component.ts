d45e4b6c9528babaebee6c1fd37966710569ecea
rohithv07 <rohithv63@gmail.com>
2021-04-06 09:40:43 +0530

    Added

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-server',
+	templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
+
+// now configure in app.module.ts
